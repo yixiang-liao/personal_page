70c64daa1234eb4ea4cbec3d778d4015d354a86d
廖翊翔 <82229244+yixiang-liao@users.noreply.github.com>
2025-07-10 20:03:28 +0800

    first commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,20 @@
 // routes/index.jsx
-import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import Acknowledgements from '../pages/Acknowledgements';
-import Font from '../pages/Font';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import Acknowledgements from "../pages/Acknowledgements";
+import Font from "../pages/Font";
+import ScrollToTop from "../services/ScrollToTop";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/acknowledgements" element={<Acknowledgements />} />
-      <Route path="/font" element={<Font />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/acknowledgements" element={<Acknowledgements />} />
+        <Route path="/font" element={<Font />} />
+      </Routes>
+    </>
   );
 };
 
